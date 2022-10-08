832154ec4a8b1c50316385b0cbf6e9dde6e4cf40
amishalodha <lodhaamisha5@gmail.com>
2022-10-08 11:42:43 +0530

    add nest-intro file

---
+++ src/users/users.controller.ts
@@ -0,0 +1,56 @@
+import { Body, Controller, Get, Post, Param , Patch , Delete } from "@nestjs/common";
+
+import { UsersService } from "./users.service";
+
+@Controller('users')
+export class UserController{
+    constructor( private readonly userService:UsersService){}
+
+// @Get()
+// getUsers(){
+//     return 'hello world';
+// }
+
+@Post()
+insertUser(
+    @Body('name') name:string, 
+    @Body('email') email:string, 
+    @Body('contact') contact:number, 
+  
+){
+
+  const userId  =this.userService.insertUser(name , email, contact);
+     return {
+        id:userId,
+     };
+}
+
+
+@Get()
+getAllUsers(){
+    return this.userService.getUsers();
+}
+
+@Get(':userId')
+getUser(@Param('userId') userId: string)
+{
+    return this.userService.getUser(userId);
+}
+   @Patch(':userId')
+   updateUser(
+    @Param('userId') userId:string,
+    @Body('name') name:string,
+    @Body('email') email:string,
+    @Body('contact') contact: number,
+   ){
+       this.userService.updateUser(userId, name , email , contact);
+          return null;
+    }
+
+
+   @Delete(':userId')
+   deleteUser(@Param('userId') userId:string){
+    this.userService.deleteUser('userId');
+    return null;
+   }
+}
---
+++ src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { UserController } from "./users.controller";
+
+import { Module } from "@nestjs/common";
+import { UsersService } from "./users.service";
+
+@Module({
+    controllers:[UserController],
+    providers:[UsersService],
+})
+
+
+export class UsersModule{}
